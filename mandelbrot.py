7c4883623013ced228a35634215a8d5f55e67c04
wheathins <46090400+wheathins@users.noreply.github.com>
2019-09-10 19:14:51 -0500

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -85,4 +85,87 @@ def loopy_mandelbrot_set(res_c, res, iterations):
     print("The Scaling Factor is:", scaling_constant)
     
 
-loopy_mandelbrot_set(10, 50, 1000000)
+
+def loopy_mandelbrot_boundary(res_c, res, iterations):
+    #point_c is a var used to track the amount of points in the set for a given plot
+    point_c = 0
+    #big is a dummy var used to keep track of the initial plot resolution
+    big = res_c
+    
+    #this first while loop increases the resolution of the mandelbrot set plot by one each iteration
+    while res_c <= res:
+        real_c = 0
+        imag_c = 0
+        step = 4/res_c
+        z = 0
+        
+        #these two while loops over the complex plane to test all the points between -2 and 2 in the real axis and -2 and 2 on the imaginary axis
+        while imag_c <= res_c:
+            while real_c <= res_c:
+                z = 0
+                real1 = (2-(step*real_c))
+                imag1 = (2-(step*imag_c))
+                con1 = complex(real, imag)
+                
+                #this for loop tests whether the points created in the loops above are in the mandelbrot set
+                #the if, elif, else logic inside the loop adds points in the set to the point_c var
+                for n in range(iterations):
+                    z = z*z + con
+                    if abs(z) >= 2:
+                        break
+                    
+                    elif abs(z) < 2 and n == iterations - 1:
+                        point_c = point_c + 1
+                        print("Found one!", res_c, abs(z))
+                        break
+
+                    else:
+                        pass
+
+                real_c = real_c + 1
+
+            real_c = 0
+            imag_c = imag_c + 1
+            
+        #the if condition creates the arrays used to store the scaling factor and number of points in the mandelbrot for each resolution iteration
+        #the log is taken of the x var so it become linear
+        if res_c == big:
+            x = np.array([m.sqrt((res_c ** 2)/(big ** 2))])
+            y = np.array([m.log10(point_c)])
+
+        #the else condition adds the values the arrays created during the first iteration of the resolution loop
+        else:
+           x = np.append(x, [m.sqrt((res_c ** 2)/(big ** 2))], axis = 0)
+           y = np.append(y, [m.log10(point_c)], axis=0)
+
+        res_c = res_c + 1
+        point_c = 0
+        
+    #reshape the arrays so the sklearn library can perform a linear regression
+    x = x.reshape((-1,1))
+    y = y.flatten()
+    print(x, y)
+    print(x.shape, y.shape)
+    
+    #call and perform a linear regression on the x and y arrays
+    model = LinearRegression()
+    model.fit(x, y)
+    r_sq = model.score(x, y)
+    #print the results
+    #the slope of the line is the scaling factor, aka, the hausdorff dimension
+    #the intercept is the is the scaling constant, an example for a scaling factor is pi. Pi is the scaling factor a circle. 
+    print('r^2:', r_sq)
+    print("The equation is: y=", model.coef_, "x +", model.intercept_)
+    #the scaling factor and scaling constant must be raised the 10 ^ to undo the log taken used to make the relationship linear
+    scaling_factor = 10 ** model.coef_
+    scaling_constant = 10 ** model.intercept_
+    print("The Dimenionsality is:", scaling_factor)
+    print("The Scaling Factor is:", scaling_constant)
+    
+    
+
+
+def paralleized_mandelbrot_set(res_c, res, iterations):
+    print(res_c, res, iterations)
+    
+paralleized_mandelbrot_set(1,2,3)
