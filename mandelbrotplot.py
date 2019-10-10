6e80e5eb6616e623689d6f4b87ea1f41819b7db0
wheathins <46090400+wheathins@users.noreply.github.com>
2019-10-10 11:42:29 -0500

    Update mandelbrotplot.py

--- mandelbrotplot.py
+++ mandelbrotplot.py
@@ -4,24 +4,39 @@ import matplotlib
 from tqdm import tqdm
 
 def mandelbrot_plot_cpu(res, iterations):
-    x = cp.linspace(-2, 2, num=res).reshape((1, res))
-    y = cp.linspace(-2, 2, num=res).reshape((res, 1))
-    C = cp.tile(x, (res, 1)) + 1j * cp.tile(y, (1, res))
+    x = np.linspace(-2, 2, num=res).reshape((1, res))
+    y = np.linspace(-2, 2, num=res).reshape((res, 1))
+    C = np.tile(x, (res, 1)) + 1j * np.tile(y, (1, res))
 
-    Z = np.zeros((res, res), dtype='complex16')
+    Z = np.zeros((res, res), dtype=complex)
     M = np.full((res, res), True, dtype=bool)
 
     for i in tqdm(range(iterations)):
         Z[M] = Z[M] * Z[M] + C[M]
         M[np.abs(Z) > 2] = False
 
-    M = np.uint8(np.flipud(M-1) * 265)
+    M_cpu = M
+    m_b = M = np.full((res, res), False, dtype=bool)
 
-    print(M.shape)
-    print(M.dtype)
-    print(M)
-    matplotlib.image.imsave('mandelbrot.png', M)
+    for i in tqdm(range(res-1)):
+        for j in range(res-1):
+            a = M_cpu[i, j]
+            b = M_cpu[(i + 1), j]
+            c = M_cpu[i, (j + 1)]
+            d = M_cpu[(i + 1), (j + 1)]
+            if (a != b) or (a != c) or (a != d) or (b != c) or (b != d) or (c != d):
+                m_b[i,j] = True
 
+            else:
+                pass
+    print(m_b)
+    print(M_cpu)
+
+    m_b = m_b.astype('int8')
+    m_b = np.flipud(1-m_b) * 265
+
+
+    matplotlib.image.imsave('mandelbrotboundary1.png', m_b)
 
 def mandelbrot_plot_gpu(res, iterations):
     mempool = cp.get_default_memory_pool()
@@ -40,13 +55,29 @@ def mandelbrot_plot_gpu(res, iterations):
         M[cp.abs(Z) > 2] = False
 
     M_cpu = cp.asnumpy(M)
-    M_cpu = M_cpu.astype('int8')
-    M_cpu = np.flipud(1-M_cpu) * 265
+    m_b = M = np.full((res, res), False, dtype=bool)
+
+    for i in tqdm(range(res-1)):
+        for j in range(res-1):
+            a = M_cpu[i, j]
+            b = M_cpu[(i + 1), j]
+            c = M_cpu[i, (j + 1)]
+            d = M_cpu[(i + 1), (j + 1)]
+            if (a != b) or (a != c) or (a != d) or (b != c) or (b != d) or (c != d):
+                m_b[i,j] = True
+
+            else:
+                pass
+    print(m_b)
+    print(M_cpu)
+
+    m_b = m_b.astype('int8')
+    m_b = np.flipud(1-m_b) * 265
 
 
-    matplotlib.image.imsave('mandelbrot.png', M_cpu)
+    matplotlib.image.imsave('mandelbrotboundary.png', m_b)
 
     mempool.free_all_blocks()
 
 
-mandelbrot_plot_gpu(5000, 10000000)
+mandelbrot_plot_cpu(10000, 64)
